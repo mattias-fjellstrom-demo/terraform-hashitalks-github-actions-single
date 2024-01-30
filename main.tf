92560cee90e5be5c7faf633d7c344280bb9db2e0
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-01-30 09:51:25 +0100

    Add version constraint for azurerm

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = "~> 1.7.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "3.88.0"
     }
   }
 }
